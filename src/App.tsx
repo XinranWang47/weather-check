204f4ec987299694ec5716093050bbfcc8b8efa0
xrhywww <admin@Admins-MacBook-Air-2.local>
2025-04-17 12:24:10 +0100

    Add loading and error message handling

--- src/App.tsx
+++ src/App.tsx
@@ -20,15 +20,28 @@ interface WeatherData {
 function App() {
   const [city,setCity] = useState<string>('')
   const [weatherData,setWeatherData] = useState<WeatherData | null>(null)
+  const [isLoading,setIsLoading] = useState<boolean>(false)
+  const [error,setError] = useState<string | null>(null)
 
   async function getWeather(){
     if (city.trim() !== ''){
+      setIsLoading(true)
+      setError(null)
+      setWeatherData(null)
+
       try{
         const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ea2c189c49928a6881e425086f1b94fd&units=metric`)
+        
+        if(!result.ok){
+          throw new Error('City not found!')
+        }
+
         const data = await result.json()
         setWeatherData(data)
-      }catch(err){
-        console.error(err);
+      }catch(err: any){
+        setError(err.message || 'Something went wrong')
+      }finally{
+        setIsLoading(false)
       }
     }
   }
@@ -38,6 +51,9 @@ function App() {
       <input placeholder='Enter a city' value={city} onChange={(e)=>setCity(e.target.value)}/>
       <button onClick={getWeather}>Search</button>
     </div>
+
+      {isLoading && <p>Loading...Please wait...</p>}
+      {error && <p style={{color:'red',fontSize:'30px'}}>{error}</p>}
       {weatherData && (
         <div className='weather-show-area'>
         <span style={{fontSize:'45px',marginTop:'50px'}}>{weatherData.name}</span>
